f42bd294370fa01f9f030f594fea5f3ac1d7f589
shilrey <joniesr_tw@yahoo.com.tw>
2024-02-20 20:25:39 +0800

    add statusbar

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,54 @@
 slint::slint! {
-    import { VerticalBox, LineEdit, ScrollView } from "std-widgets.slint";
+    import { VerticalBox, LineEdit, ScrollView, HorizontalBox } from "std-widgets.slint";
+    struct Status {
+        date-time: string,        
+        health-power: int,
+        health: string,
+    }
+    component StatusBar inherits Rectangle {
+        background: lightblue;
+        in property <Status> status;
+        HorizontalBox {
+            padding: 2px;
+            Text {
+                text: status.date-time;
+                color: yellow;
+                font-size: 9px;
+            }
+            Text {
+                text: status.health-power;
+                font-size: 9px;
+            }
+            Text {
+                text: status.health;
+                color: status.health-power < 20? red: status.health-power < 50? yellow: white;
+                font-size: 9px;
+            }
+        }
+    }
     export  component RoundBorder inherits Rectangle {
         border-color: gray;
         border-width: 1px;
         border-radius: 5px;        
     }
-export component MainConsole inherits Window {
-        in property <int> width_;
-        in property <int> height_;
+    export component MainConsole inherits Window {
+        in property <int> width_: 400;
+        in property <int> height_: 300;
         width: width_ * 1px;
         height: max(200, height_) * 1px;
         in-out property <string> content;
+        in property <Status> status: {
+            date-time: "12:00:01",
+            health-power: 100,
+            health: "Normal",
+         };
         background: white;
         VerticalBox {
+            StatusBar {
+                status: status;
+            }
             RoundBorder {
                 width: parent.width - parent.padding * 2;
-                height: parent.height - 50px;
                 clip: true;
                 ScrollView {
                     width: parent.width;
@@ -52,10 +85,7 @@ export component MainConsole inherits Window {
 }
 
 fn main() {
-    println!("Hello, world!");
     if let Ok(ui) = MainConsole::new() {
-        ui.set_width_(400);
-        ui.set_height_(300);
         ui.set_content("Hello!! Welcome to this wolrd!\n".into());
         let _ = ui.run();
     }
